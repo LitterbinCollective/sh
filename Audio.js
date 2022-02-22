0ee6f82649874456abdf45cdb3429bad39d55452
be195 <ilya.wicopee@gmail.com>
2022-02-22 07:31:13 +0300

    Misc. changes

--- Audio.js
+++ Audio.js
@@ -1,4 +1,28 @@
-// todo: implement mods with ffmpeg
 module.exports = class Audio {
+  constructor () {
+    this.modifiers = {};
+  }
 
+  scriptToTimeline(script) {
+    const timeline = [];
+    const parseWord = function(word, mods) {
+      for (const wordOrLink of word.words) {
+        switch (typeof wordOrLink) {
+          case 'string':
+            timeline.push({
+              link: wordOrLink,
+              mods: word.mods.concat(mods || [])
+            });
+            break;
+          case 'object':
+            parseWord(wordOrLink, word.mods.concat(mods || []));
+            break;
+        }
+      }
+    };
+
+    for (let i = 0; i < script.length; i++)
+      parseWord(script[i]);
+    return timeline;
+  }
 }
--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+const Audio = require('./Audio');
 const Parser = require('./Parser');
 
 module.exports = class Sh {
   constructor (list) {
+    this.Audio = new Audio();
     this.Parser = new Parser(list);
   }
 }
+
+const sh = new module.exports(require('./shat.json'));
+const script = sh.Parser.parse('fuck you:lfopitch(2, 5) (shit:cut(10) ass):echo(1) hi:pitch(5)');
+console.log(script);
+console.log(sh.Audio.scriptToTimeline(script));
