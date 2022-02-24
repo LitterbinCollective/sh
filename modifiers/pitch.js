ce30615f4f16641a8d092528e919dee41a22d479
be195 <ilya.wicopee@gmail.com>
2022-02-24 06:11:03 +0300

    Use adelay instead of concat

--- modifiers/pitch.js
+++ modifiers/pitch.js
@@ -1,3 +1,9 @@
-module.exports = function(args) {
-  return '[{0}]asetrate=' + (48000 * Number(args[0])) + '[{1}]';
+module.exports = function(args, delay) {
+  let num = Number(args[0]);
+  if (isNaN(num) || !isFinite(num))
+    num = 1;
+  return {
+    filter: '[{0}]asetrate=' + (48000 * num) + '[{1}]',
+    delay: delay / num
+  };
 }
