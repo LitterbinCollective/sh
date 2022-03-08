438a45acc53f432f9a12a748b84692f204e42b9e
be195 <ilya.wicopee@gmail.com>
2022-03-09 01:21:03 +0300

    Experimental: fix Parser, again!

--- test.js
+++ test.js
@@ -1,17 +1,10 @@
 const Sh = require('.');
-const Speaker = require('speaker');
 
 const sh = new Sh(require('./shat.json'));
 
 process.stdin.on('data', async (data) => {
   data = data.toString().trim();
 
-  const speaker = new Speaker({
-    channels: 2,
-    bitDepth: 16,
-    sampleRate: 48000
-  });
   const script = sh.Parser.parse(data);
-  const stream = await sh.Audio.run(script);
-  stream.pipe(speaker);
+  console.log(script);
 })
