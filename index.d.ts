f7e27452910836c819665890260f2527eeec75df
be195 <ilya.wicopee@gmail.com>
2022-03-08 20:20:17 +0300

    Add types, import modifiers individually

---
+++ index.d.ts
@@ -0,0 +1,14 @@
+// Too lazy to implement more stricter types.
+
+declare class Parser {
+  public parse(string: string): any;
+}
+
+declare class Audio {
+  public run(script: any): any;
+}
+
+declare class Sh {
+  public Audio: Audio;
+  public Parser: Parser;
+}
