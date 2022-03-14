4b192858298caae865b6594d76d6a59ba30cc74b
Ilya <wicobotdiscord@gmail.com>
2022-03-15 00:52:41 +0300

    Fix negative pitch for browser version

--- modifiers/pitch.js
+++ modifiers/pitch.js
@@ -10,18 +10,37 @@ module.exports.njs = function ([ pitch ], delay) {
   };
 }
 
-module.exports.browser = function ([ pitch ], delay, _, input) {
+module.exports.browser = function ([ pitch ], delay, _, input, ctx) {
   pitch = Number(pitch);
   if (isNaN(pitch) || !isFinite(pitch))
     pitch = 1;
   if (pitch === 0) return {};
-  if (pitch < 0) input.buffer.reverse();
-  console.log(pitch, input);
+  if (pitch < 0) {
+    const reverse = ctx.createBuffer(
+      input.buffer.numberOfChannels,
+      input.buffer.length,
+      input.buffer.sampleRate
+    );
 
-  input.playbackRate.value *= pitch;
+    for (let c = 0; c < input.buffer.numberOfChannels; c++) {
+      const dest = reverse.getChannelData(c);
+      const src = input.buffer.getChannelData(c);
+
+      for (let samples = 0; samples < input.buffer.length; samples++)
+        dest[samples] = src[input.buffer.length - samples];
+    }
+
+    const lS = input.loopStart;
+    input = ctx.createBufferSource();
+    input.buffer = reverse;
+    input.loop = true;
+    input.loopEnd = lS;
+  }
+
+  input.playbackRate.value *= Math.abs(pitch);
 
   return {
-    delay: delay / pitch,
+    delay: delay / Math.abs(pitch),
     node: input
   };
 }
